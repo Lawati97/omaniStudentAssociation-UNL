116a2d06e4fd23b3b38ffca249fdbd6100ad5033
Lawati97 <hussain_97@live.com>
2019-12-24 17:06:40 -0600

    Pushing all of the code to github

---
+++ README.md
@@ -0,0 +1,1 @@
+# omaniStudentAssociation-UNL
---
+++ app.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+
+app.get("/", function(request, respond){
+respond.sendFile(__dirname + "/index.html");
+});
+
+
+
+app.listen(3000, function(){
+console.log("Server started on port 3000");
+});
